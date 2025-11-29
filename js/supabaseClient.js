95eb8db475cf805b3f45062a6c55df9143451759
Ray <raymorakatielly@gmail.com>
2025-11-29 00:16:34 -0300

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 // URL do projeto e chave pública do Supabase
-const supabaseUrl = 'db.kahnknadtbtacghxjgrk.supabase.co'
+const supabaseUrl = 'https://kahnknadtbtacghxjgrk.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImthaG5rbmFkdGJ0YWNnaHhqZ3JrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzNTkwMDksImV4cCI6MjA3OTkzNTAwOX0.CAP-39Sd7gv2uR7Sh_dt0WN2PfZsetcgimEa--V8tgs'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
